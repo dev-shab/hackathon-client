a04340008d72b52035a420f5e1b3e763effcfd03
dev-shab <shabreeshnair1998@gmail.com>
2025-03-17 09:28:27 +0530

    materialUI + tailwindCSS setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-3xl">
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
 };
 
 export default App;
